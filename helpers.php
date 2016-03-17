574f43b197e6659078ce1aed209a1868b822c06e
Webdev <webdev@virtualbox.com>
2016-03-17 15:07:37 +0100

    change structure to helpers

--- helpers.php
+++ helpers.php
@@ -1,17 +1,13 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: webdev
+ * User: lucaSartori
  * Date: 17-3-16
- * Time: 14:52
+ * Time: 15:05
  */
 
-namespace LeisureUtil;
-
-
-class Util
-{
-    public static function sortArray($testArray)
+if (! function_exists('sortArray')) {
+    function sortArray($testArray)
     {
         $coll = collator_create('fr_FR');
         $coll->sort($testArray);
